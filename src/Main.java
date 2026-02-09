bc90cf001eb17d1e2777cf7290f7abd1cf6089d8
sphurti <45139765+meerathorat@users.noreply.github.com>
2026-02-09 10:51:54 +0530

    Some new additions

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        int ans;
         System.out.printf("Hello and welcome!");
         System.out.printf("Jenkins Checking Poll SCM!");
+        ans = 5 +2;
+        System.out.printf("Addition!"+ans);
+
 
 
     }
